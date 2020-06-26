ea29c266c593206227e9653fd00387c377e73f22
Thanh Tuấn <thanhtuanhk13@gmail.com>
2020-06-26 08:59:47 +0700

    update_database

--- QLVDKHPVTHPSV.sql
+++ QLVDKHPVTHPSV.sql
@@ -1,175 +1,143 @@
-CREATE DATABASE QLVDKHPVTHPSV
+﻿CREATE DATABASE QLVDKHPVTHPSV
 USE QLVDKHPVTHPSV
 
-CREATE TABLE Khoa
+CREATE TABLE DIAPHUONG
 (
-	MaKhoa varchar(100) PRIMARY KEY,
-	TenKhoa varchar(100),
+	MaHuyen varchar(2),
+	MaTinh varchar(2),
+	TenHuyen varchar(50),
+	TenTinh varchar(50),
+	VungSau bit,
+	PRIMARY KEY (MaHuyen, MaTinh)	
+);
+
+CREATE TABLE KHOA
+(
+	MaKhoa varchar(8) PRIMARY KEY,
+	TenKhoa varchar(50),
 )
 
-CREATE TABLE Nganh
+CREATE TABLE NGANH
 (
-	MaNganh varchar(100) PRIMARY KEY,
-	TenNganh varchar(100),
-	MaKhoa varchar(100),
+	MaNganh varchar(8) PRIMARY KEY,
+	TenNganh varchar(50),
+	MaKhoa varchar(8)
 	CONSTRAINT FK_Khoa_Nganh FOREIGN KEY (MaKhoa) REFERENCES Khoa(MaKhoa),
 )
-CREATE TABLE DoiTuong
+
+
+
+CREATE TABLE DOITUONG
 (
-	MaDoiTuong varchar(100) PRIMARY KEY,
-	TenDoiTuong varchar(100),
-	TyLeGiam int,
+	MaDoiTuong varchar(2) PRIMARY KEY,
+	TenDoiTuong varchar(50),
+	TyLeGiam decimal(3, 2),
 )
 
-CREATE TABLE Tinh
-(
-	MaTinh varchar(100) PRIMARY KEY,
-	TenTinh varchar(100),
-)
-
-CREATE TABLE Huyen
-(
-	MaHuyen varchar(100) PRIMARY KEY,
-	TenHuyen varchar(100),
-	MaTinh varchar(100) 
-	CONSTRAINT FK_Tinh_Huyen FOREIGN KEY (MaTinh) REFERENCES Tinh(MaTinh), 
-)
-
-CREATE TABLE DiaPhuong
-(
-	MaDiaPhuong varchar(100) PRIMARY KEY,
-	VungXauVungXa bit, 
-	MaTinh varchar(100) 
-	CONSTRAINT FK_Tinh_DiaPhuong FOREIGN KEY (MaTinh) REFERENCES Tinh(MaTinh),
-	MaHuyen varchar(100),
-	CONSTRAINT FK_Huyen_DiaPhuong FOREIGN KEY (MaHuyen) REFERENCES Huyen(MaHuyen),
-)
 CREATE TABLE SinhVien
 (
-	MaSV varchar(100) PRIMARY KEY,
-	HoTen varchar(100),
+	MaSV varchar(8) PRIMARY KEY,
+	HoTen varchar(50),
 	NgaySinh smalldatetime,
-	GioiTinh varchar(100),
-	MaDiaPhuong varchar(100),
-	CONSTRAINT FK_DiaPhuong_SV FOREIGN KEY (MaDiaPhuong) REFERENCES DiaPhuong(MaDiaPhuong),
-	MaNganh varchar(100),
+	GioiTinh bit,
+	MaHuyen varchar(2),
+	MaTinh varchar(2),
+	CONSTRAINT FK_DiaPhuong_SV FOREIGN KEY (MaHuyen,MaTinh) REFERENCES DiaPhuong(MaHuyen,MaTinh),
+	MaNganh varchar(8),
 	CONSTRAINT FK_Nganh_SV FOREIGN KEY (MaNganh) REFERENCES Nganh(MaNganh),
-	MaKhoa varchar(100),
-	CONSTRAINT FK_Khoa_SV FOREIGN KEY (MaKhoa) REFERENCES Khoa(MaKhoa),
-	MaDoiTuong varchar(100),
+	MaDoiTuong varchar(2),
 	CONSTRAINT FK_DoiTuong_SV FOREIGN KEY (MaDoiTuong) REFERENCES DoiTuong(MaDoiTuong)
 )
-CREATE TABLE LoaiMon
+
+CREATE TABLE MONHOC
 (
-	MaLoaiMon varchar(100) PRIMARY KEY,
-	TenLoaiMon varchar(100),
+	MaMH varchar(5) PRIMARY KEY,
+	TenMH varchar(50),
+	LoaiMon varchar(2), --LT hoặc TH
+	SoTiet tinyint,
+	SoTC tinyint
 )
-CREATE TABLE MonHoc
+
+CREATE TABLE CTHOC
 (
-	MaMonHoc varchar(100) PRIMARY KEY,
-	TenMonHoc varchar(100),
-	SoTiet int,
-	SoTinChi int,
-	MaLoaiMon varchar(100),
-	CONSTRAINT FK_LoaiMon_MH FOREIGN KEY (MaLoaiMon) REFERENCES LoaiMon(MaLoaiMon),
-	HocPhiMon money,
-)
-CREATE TABLE CTHoc
-(
-	MaCTH varchar(100) PRIMARY KEY,
-	MaNganh varchar(100),
+	MaCTH varchar(4) PRIMARY KEY,
+	MaNganh varchar(8),
+	MaKhoa varchar(8),
 	CONSTRAINT FK_Nganh_CTHoc FOREIGN KEY (MaNganh) REFERENCES Nganh(MaNganh),
-	MaKhoa varchar(100),
 	CONSTRAINT FK_Khoa_CTHoc FOREIGN KEY (MaKhoa) REFERENCES Khoa(MaKhoa),
 )
-CREATE TABLE CT_CTHoc
+
+CREATE TABLE CT_CTHOC
 (
-	MaCT_CTHoc varchar(100) PRIMARY KEY,
-	HocKy int,
-	MaMonHoc varchar(100),
-	CONSTRAINT FK_MonHoc_CTCTHoc FOREIGN KEY (MaMonHoc) REFERENCES MonHoc(MaMonHoc),
-	MaCTH varchar(100),
-	CONSTRAINT FK_CTH_CTCTHoc FOREIGN KEY (MaCTH) REFERENCES CTHoc(MaCTH),
+	MaCTH varchar(4),
+	MaMH varchar(5),
+	HocKy tinyint,
+	GhiChu varchar(50),
+	PRIMARY KEY (MaCTH, MaMH),
+	CONSTRAINT FK_MaCTH FOREIGN KEY (MaCTH) REFERENCES CTHOC(MaCTH),
+	CONSTRAINT FK_MaMH FOREIGN KEY (MaMH) REFERENCES MONHOC(MaMH),
 )
-CREATE TABLE HocKyNH
+
+CREATE TABLE DS_MONHOCMO
 (
-	MaHKNH varchar(100) PRIMARY KEY,
-	TenHKNH varchar(100),
+	MaDSMHM varchar(4) PRIMARY KEY,
+	HocKy tinyint,
+	NamHoc varchar(4),
 )
-CREATE TABLE MonHocMo
+
+CREATE TABLE CT_MONHOCMO
 (
-	MaDSMonHocMo varchar(100) PRIMARY KEY,
-	MaHKNH varchar(100)
-	CONSTRAINT FK_HKNH_MonHocMo FOREIGN KEY (MaHKNH) REFERENCES HocKyNH(MaHKNH),
-	NamHoc varchar(100),
-	MaMonHoc varchar(100) REFERENCES MonHoc(MaMonHoc),
-	CONSTRAINT FK_MonHoc_MonHocMo FOREIGN KEY (MaMonHoc) REFERENCES MonHoc(MaMonHoc),
-	MaCT_CTHoc varchar(100),
-	CONSTRAINT FK_CTCTHoc_MonHocMo FOREIGN KEY (MaCT_CTHoc) REFERENCES CT_CTHoc(MaCT_CTHoc)
+	MaCTH varchar(4),
+	MaDSMHM varchar(4),
+	PRIMARY KEY (MaCTH, MaDSMHM),
+	CONSTRAINT FK_MaCTH_MHM FOREIGN KEY (MaCTH) REFERENCES CTHOC(MaCTH),
+	CONSTRAINT FK_MaDSMHM FOREIGN KEY (MaDSMHM) REFERENCES DS_MONHOCMO(MaDSMHM),
 )
 
 CREATE TABLE DKHP
 (
-	MaDKHP varchar(100) PRIMARY KEY,
-	MaSV varchar(100),
-	CONSTRAINT FK_SinhVien_DKHP FOREIGN KEY (MaSV) REFERENCES SinhVien(MaSV),
+	MaDKHP varchar(4) PRIMARY KEY,
+	MaSV varchar(8),
 	NgayLap smalldatetime,
-	HocKyDKHP int,
-	NamHoc varchar(100),
-	MaHKNH varchar(100),
-	CONSTRAINT FK_HKNH_DKHP FOREIGN KEY (MaHKNH) REFERENCES HocKyNH(MaHKNH),
-	HocPhiDKHP money,
+	HocKy tinyint,
+	NamHoc varchar(4),
+	CONSTRAINT FK_MaSV FOREIGN KEY (MaSV) REFERENCES SINHVIEN(MaSV),
 )
-CREATE TABLE CTDKHP
-(	
-	MaCTDKHP varchar(100) PRIMARY KEY,
-	MaDKHP varchar(100),
-	CONSTRAINT FK_DKHP_CTDKHP FOREIGN KEY (MaDKHP) REFERENCES DKHP(MaDKHP),
-	MaMH varchar(100),
-	CONSTRAINT FK_MonHoc_CTDKHP FOREIGN KEY (MaMH) REFERENCES MonHoc(MaMonHoc),
-	HocPhiMonHoc money,
-)	
-CREATE TABLE PhieuThu
+
+CREATE TABLE CT_DKHP
 (
-	MaPhieuThu varchar(100) PRIMARY KEY,
-	MaSV varchar(100),
-	CONSTRAINT FK_SinhVien_PhieuThu FOREIGN KEY (MaSV) REFERENCES SinhVien(MaSV),
-	TienThu money,
-	NgayLapPhieuThu smalldatetime,
-	MaDKHP varchar(100),
-	CONSTRAINT FK_DKHP_PhieuThu FOREIGN KEY (MaDKHP) REFERENCES DKHP(MaDKHP)
+	MaCT_DKHP varchar(4) PRIMARY KEY,
+	MaDKHP varchar(4),
+	MaMH varchar(5),
+	CONSTRAINT FK_DKHP_MaMH FOREIGN KEY (MaMH) REFERENCES MONHOC(MaMH),
 )
-CREATE TABLE LichSuDongHP
+
+CREATE TABLE PHIEUTHU_HP
 (
-	MaLSDongHP varchar(100) PRIMARY KEY,
-	MaPhieuThu varchar(100),
-	CONSTRAINT FK_PhieuThu_LSDongHP FOREIGN KEY (MaPhieuThu) REFERENCES PhieuThu(MaPhieuThu),
-	MaSV varchar(100),
-	CONSTRAINT FK_SinhVien_LSDongHP FOREIGN KEY (MaSV) REFERENCES SinhVien(MaSV),
+	MaPhieu varchar(4) PRIMARY KEY,
+	MaDKHP varchar(4),
+	MaSV varchar(8),
+	NgayLap smalldatetime,
+	SoTienThu money,
+	CONSTRAINT FK_PhieuThu_DKHP FOREIGN KEY (MaDKHP) REFERENCES DKHP(MaDKHP),
+	CONSTRAINT FK_PhieuThu_SV FOREIGN KEY (MaSV) REFERENCES SINHVIEN(MaSV),
+)
+
+CREATE TABLE DS_CHUADONGHP
+(
+	MaDS varchar(4) PRIMARY KEY,
+	HocKy tinyint,
+	NamHoc varchar(4),
+)
+
+CREATE TABLE CT_DS_CHUADONGHP
+(
+	MaCT varchar(4) PRIMARY KEY,
+	MaDS varchar(4),
+	MaSV varchar(8),
+	SoTienDK money,
 	SoTienDaDong money,
-)
-
-CREATE TABLE DSChuaDongHP
-(
-	MaDSChuaDongHP varchar(100) PRIMARY KEY,
-	MaHKNH varchar(100),
-	CONSTRAINT FK_HocKyNH_DSChuaDongHP FOREIGN KEY (MaHKNH) REFERENCES HocKyNH(MaHKNH),
-	NamHoc varchar(4)
-)
-
-
-CREATE TABLE CT_DSChuaDongHP
-(
-	MaCTDSChuaDongHP varchar(100) PRIMARY KEY,
-	MaDSChuaDongHP varchar(100),
-	CONSTRAINT FK_DSChuaDongHP_CTDSChuaDongHP FOREIGN KEY (MaDSChuaDongHP) REFERENCES DSChuaDongHP(MaDSChuaDongHP),
-	SoTienPhaiDong money,
-	MaDKHP varchar(100),
-	CONSTRAINT FK_DKHP_CTDSChuaDongHP FOREIGN KEY (MaDKHP) REFERENCES DKHP(MaDKHP),
-	MaLSDongHP varchar(100),
-	CONSTRAINT FK_LSDongHp_CTDSChuaDongHP FOREIGN KEY (MaLSDongHP) REFERENCES LichSuDongHP(MaLSDongHP),
 	SoTienConLai money,
-	MaSV varchar(100),
-	CONSTRAINT FK_SinhVien_CTDSChuaDongHP FOREIGN KEY (MaSV) REFERENCES SinhVien(MaSV),
+	CONSTRAINT FK_DS FOREIGN KEY (MaDS) REFERENCES DS_CHUADONGHP(MaDS),
+	CONSTRAINT FK_MaSV_DS FOREIGN KEY (MaSV) REFERENCES SINHVIEN(MaSV),
 )
-
