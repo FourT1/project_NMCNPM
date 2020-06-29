e57a8ef3dcf20ee8385222f61f5b84823c5ac417
TheHung <phungthehung1610@gmail.com>
2020-06-29 16:07:30 +0700

    .

--- test/WindowsFormsApp1/DSSVChuaDongHp.cs
+++ test/WindowsFormsApp1/DSSVChuaDongHp.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
 
 namespace ChuaDongHP
 {
@@ -15,6 +18,33 @@ namespace ChuaDongHP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection(@"Data Source=.\THEHUNG1610;Initial Catalog=QLVDKHPVTHPSV;Integrated Security=True");
+        private void lbHocKy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbHocKy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int MaHK = Convert.ToInt32(cbHocKy.SelectedValue);
+            var dap = new SqlDataAdapter("SELECT MaNH AS NĂM_HỌC ,MaSV ,SoTienDK AS SỐ_TIỀN_ĐĂNG_KÝ ,SoTienDaDong AS SỐ_TIỀN_ĐÃ_ĐÓNG,SoTienConLai AS SỐ_TIỀN_CÒN_LẠI FROM CT_DS_CHUADONGHP WHERE MaHK = " + MaHK + " ", connection);
+            var table = new DataTable();
+            dap.Fill(table);
+            dtgvChuaDongHP.DataSource = table;
+            dtgvChuaDongHP.AllowUserToAddRows = false;
+            dtgvChuaDongHP.EditMode = DataGridViewEditMode.EditProgrammatically;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var dap = new SqlDataAdapter("SELECT * FROM HocKy ", connection);
+            var table = new DataTable();
+            dap.Fill(table);
+            cbHocKy.DisplayMember = "HocKy";
+            cbHocKy.ValueMember = "MaHK";
+            cbHocKy.DataSource = table;
         }
     }
 }
